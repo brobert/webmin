9f09fbb4c7a47e2861b296f6fff748e6fc176da6
brobert <robert.blonski+git@gmail.com>
2018-09-10 07:30:39 +0200

    chat message seeder v1

--- database/seeds/ChatSeeder.php
+++ database/seeds/ChatSeeder.php
@@ -8,18 +8,32 @@ class ChatSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        factory(App\Models\Chat::class, 50)->create()->each(function ($chat) {
-            $faker = Faker::create();
+        $faker = Faker::create();
+        
+        factory(App\Models\Chat::class, 50)->create()->each(function ($chat) use ($faker) {
+            
             $user_1 = $faker->numberBetween(1, 10);
             $user_2 = $faker->numberBetween(11, 50);
             $chat->users()->attach([
                 $user_1,
                 $user_2
             ]);
-
-            // $chat->messages()->saveMany(factory(App\Models\ChatMessage::class, $faker->randomDigit(0, 15))->make());
-
             $chat->save();
+            //$chat->messages()->saveMany(factory(App\Models\ChatMessage::class, $faker->randomDigit(0, 15))->make());
+            
+            factory(App\Models\ChatMessage::class, $faker->randomDigit(5, 15))->create()->each(function ($chatMessage) use ($faker, $chat, $user_1, $user_2) {
+
+                $chatMessage->chat_id = $chat->id;
+                $chatMessage->user_id = $faker->randomElement([$user_1, $user_2]);
+                
+                $chatMessage->save();
+            });
+
+            
         });
+        
+        $chats = App\Models\Chat::all();
+        echo("-------------------------------------\n");
+        echo(print_r($chats, 1));
     }
 }
